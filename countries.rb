0fe2d0d1831fd9b83bd5b56bc22f48b256a8f0c9
Christopher Terrano <mterrano1@yahoo.com>
2023-05-22 14:51:49 -0500

    create starter code in countries file

---
+++ countries.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require 'httparty'
+
+class Countries
+    def self.fetch_data
+        # Your code here
+    end
+
+    def self.transform_data(response)
+        # Your code here
+    end
+end
