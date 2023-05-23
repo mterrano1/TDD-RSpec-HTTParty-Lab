bc35643dcea8314a4813745e2eb269ba5ebb6399
Christopher Terrano <mterrano1@yahoo.com>
2023-05-23 09:26:24 -0500

    add solution

--- countries.rb
+++ countries.rb
@@ -2,11 +2,23 @@ require 'pry'
 require 'httparty'
 
 class Countries
+    # Your code here
     def self.fetch_data
-        # Your code here
+        response = HTTParty.get('https://restcountries.com/v3.1/subregion/Northern%20Europe')
+        raise 'API request failed' unless response.success?
+        response
     end
 
     def self.transform_data(response)
         # Your code here
+        response.map do |country|
+            {
+                name: country['name']['common'],
+                population: country['population'],
+                flags: country['flags']['png'],
+                languages: country['languages'],
+                capital: country['capital'][0]
+            }
+        end
     end
 end
