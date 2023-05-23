9fe737369e183d4dc2f51b7df42898096df7eebf
Christopher Terrano <mterrano1@yahoo.com>
2023-05-23 10:40:30 -0500

    modify indentations to conform to ruby style guide

--- countries.rb
+++ countries.rb
@@ -2,11 +2,11 @@ require 'pry'
 require 'httparty'
 
 class Countries
-    def self.fetch_data
-        # Your code here
-    end
+  def self.fetch_data
+    # Your code here
+  end
 
-    def self.transform_data(response)
-        # Your code here
-    end
+  def self.transform_data(response)
+    # Your code here
+  end
 end
--- spec/countries_spec.rb
+++ spec/countries_spec.rb
@@ -4,82 +4,82 @@ require 'httparty'
 require 'json'
 
 describe Countries do
-    describe '.fetch_data' do
-        context 'when the API responds successfully' do
-            before do
-                stub_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe')
-                    .to_return(status: 200, body: File.read('spec/fixtures/countries.json'), headers: {})
-            end
+  describe '.fetch_data' do
+    context 'when the API responds successfully' do
+      before do
+        stub_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe')
+          .to_return(status: 200, body: File.read('spec/fixtures/countries.json'), headers: {})
+      end
 
-            it 'hits the correct API endpoint' do
-                Countries.fetch_data
-                expect(a_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe')).to have_been_made.once
-            end
+      it 'hits the correct API endpoint' do
+        Countries.fetch_data
+        expect(a_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe')).to have_been_made.once
+      end
 
-            it 'receives a successful response' do
-                response = Countries.fetch_data
-                expect(response.code).to eq(200)
-            end
+      it 'receives a successful response' do
+        response = Countries.fetch_data
+        expect(response.code).to eq(200)
+      end
 
-            it 'returns an array of countries' do
-                response = Countries.fetch_data
-                response_array = JSON.parse(response.parsed_response)
-                expect(response_array).to be_an(Array)
-            end
+      it 'returns an array of countries' do
+        response = Countries.fetch_data
+        response_array = JSON.parse(response.parsed_response)
+        expect(response_array).to be_an(Array)
+      end
 
-            it 'countries have the expected keys' do
-                response = Countries.fetch_data
-                response_array = JSON.parse(response.parsed_response)
-                expect(response_array.first).to include("name", "population", "flags", "languages", "capital") 
-            end
-        end
-
-        context 'when the API responds with an error' do
-            before do
-                stub_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe').to_return(status: 500)
-            end
-
-            it 'raises a runtime error' do
-                expect { Countries.fetch_data }.to raise_error(RuntimeError, 'API request failed')
-            end
-        end
+      it 'countries have the expected keys' do
+        response = Countries.fetch_data
+        response_array = JSON.parse(response.parsed_response)
+        expect(response_array.first).to include("name", "population", "flags", "languages", "capital") 
+      end
     end
 
-    describe '.transform_data' do
-        let(:sample_response) { JSON.parse(File.read('spec/fixtures/countries.json')) }
+    context 'when the API responds with an error' do
+      before do
+        stub_request(:get, 'https://restcountries.com/v3.1/subregion/Northern%20Europe').to_return(status: 500)
+      end
 
-        it 'returns an array' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data).to be_an(Array)
-        end
-
-        it 'transforms the common name correctly' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data.first[:name]).to eq('Guernsey')
-        end
-
-        it 'transforms the population correctly' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data.first[:population]).to eq(62999)
-        end
-
-        it 'transforms the flags png correctly' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data.first[:flags]).to eq('https://flagcdn.com/w320/gg.png')
-        end
-
-        it 'transforms the languages correctly' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data.first[:languages]).to eq({
-                "eng" => "English",
-                "fra" => "French",
-                "nfr" => "Guernésiais"
-            })
-        end
-
-        it 'transforms the capital correctly' do
-            transformed_data = Countries.transform_data(sample_response)
-            expect(transformed_data.first[:capital]).to eq('St. Peter Port')
-        end
+      it 'raises a runtime error' do
+        expect { Countries.fetch_data }.to raise_error(RuntimeError, 'API request failed')
+      end
     end
+  end
+
+  describe '.transform_data' do
+    let(:sample_response) { JSON.parse(File.read('spec/fixtures/countries.json')) }
+
+    it 'returns an array' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data).to be_an(Array)
+    end
+
+    it 'transforms the common name correctly' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data.first[:name]).to eq('Guernsey')
+    end
+
+    it 'transforms the population correctly' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data.first[:population]).to eq(62999)
+    end
+
+    it 'transforms the flags png correctly' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data.first[:flags]).to eq('https://flagcdn.com/w320/gg.png')
+    end
+
+    it 'transforms the languages correctly' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data.first[:languages]).to eq({
+        "eng" => "English",
+        "fra" => "French",
+        "nfr" => "Guernésiais"
+      })
+    end
+
+    it 'transforms the capital correctly' do
+      transformed_data = Countries.transform_data(sample_response)
+      expect(transformed_data.first[:capital]).to eq('St. Peter Port')
+    end
+  end
 end
